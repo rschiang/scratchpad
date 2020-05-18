2136394aaca575f3cd30357438bd03c9430a17a2
Poren Chiang <ren.chiang@gmail.com>
2020-05-17 22:05:50 -0700

    Use Markdown extensions

--- Scratchpad/AppDelegate.m
+++ Scratchpad/AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import <cmark-gfm.h>
+#import <cmark-gfm-core-extensions.h>
 
 @interface AppDelegate ()
 
@@ -12,6 +13,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    cmark_gfm_core_extensions_ensure_registered();
 }
 
 
@@ -27,11 +29,31 @@
     NSDate *startTime = [[NSDate alloc] init];
 
     const char *markdownText = [[self.textView string] UTF8String];
-    int options = CMARK_OPT_HARDBREAKS | CMARK_OPT_UNSAFE | CMARK_OPT_FOOTNOTES;
-    const char *htmlText = cmark_markdown_to_html(markdownText, strlen(markdownText), options);
+
+    int options = CMARK_OPT_LIBERAL_HTML_TAG | CMARK_OPT_UNSAFE | CMARK_OPT_FOOTNOTES;
+    cmark_parser *parser = cmark_parser_new(options);
+    cmark_llist *extensions = nil;
+    char *extension_names[] = {"table", "strikethrough", "autolink", "tasklist"};
+    for (int i = 0; i < 4; i++) {
+        cmark_syntax_extension *extension = cmark_find_syntax_extension(extension_names[i]);
+        if (!extension) {
+            NSLog(@"Extension %s unavailable", extension_names[i]);
+        } else {
+            cmark_parser_attach_syntax_extension(parser, extension);
+            cmark_llist_append(cmark_get_default_mem_allocator(), extensions, extension);
+        }
+    }
+
+    cmark_parser_feed(parser, markdownText, strlen(markdownText));
+    cmark_node *document = cmark_parser_finish(parser);
+    cmark_parser_finish(parser);
+
+    const char *htmlText = cmark_render_html(document, options, extensions);
     NSString *html = [[NSString alloc] initWithUTF8String: htmlText];
     [self.webView loadHTMLString:html baseURL:nil];
+
     free((void *)htmlText);
+    cmark_llist_free_full(cmark_get_default_mem_allocator(), extensions, (cmark_free_func) cmark_syntax_extension_free);
 
     NSLog(@"Time: %.02fms", [startTime timeIntervalSinceNow] * -1000.0);
 }
